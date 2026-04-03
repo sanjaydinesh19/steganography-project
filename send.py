fa306b5f50b12a41b98bebcb9ae554eb02289cee
Sanjay Dinesh <sanjaydinesh2006@gmail.com>
2026-04-03 19:47:50 +0530

    Updated ATECC

--- send.py
+++ send.py
@@ -1,6 +1,4 @@
 import socket
-import os
-
 from Crypto.Random import get_random_bytes
 
 from crypto.aes_gcm import encrypt_data
@@ -13,37 +11,30 @@ PORT = 5000
 
 
 def main():
-    # Generate a fresh 256-bit key every run 
     key = get_random_bytes(32)
-
-    # Save key for receiver
-    with open("session.key", "wb") as f:
-        f.write(key)
 
     msg = b"Hello from Raspberry Pi via Steganography!"
 
     nonce, ct, tag = encrypt_data(msg, key)
     payload = build_payload(nonce, tag, ct)
 
-    print("Payload length:", len(payload))
-
     embed_payload("input(512x512).png", "stego.png", payload)
 
-    # Read image as bytes
     with open("stego.png", "rb") as f:
-        data = f.read()
+        img_data = f.read()
 
     print("Connecting to receiver...")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
-        length = len(data).to_bytes(4, 'big')
+        # Send key first
+        s.sendall(len(key).to_bytes(2, 'big') + key)
 
-        print("Sending image of size:", len(data))
-        s.sendall(length + data)
+        # Then send image
+        s.sendall(len(img_data).to_bytes(4, 'big') + img_data)
 
-    print("Image sent successfully.")
+    print("Image + key sent successfully.")
 
 
 if __name__ == "__main__":
