9523b85b99376ac54dae91e8aa4e835e850dcc2f
Sanjay Dinesh <sanjaydinesh2006@gmail.com>
2026-04-09 10:57:54 +0530

    Added two-way steganography communication + input images

--- receive.py
+++ receive.py
@@ -1,7 +1,9 @@
 import socket
+from Crypto.Random import get_random_bytes
 
-from crypto.aes_gcm import decrypt_data
-from utils.helpers import unpack_payload
+from crypto.aes_gcm import encrypt_data, decrypt_data
+from utils.helpers import build_payload, unpack_payload
+from stego.dct_embed import embed_payload
 from stego.dct_extract import extract_payload
 
 
@@ -30,29 +32,40 @@ def main():
         with conn:
             print("Connected by", addr)
 
-            # Receive key first
             key_len = int.from_bytes(receive_all(conn, 2), 'big')
             key = receive_all(conn, key_len)
 
-            print("Received key")
-
-            # Receive image
             img_len = int.from_bytes(receive_all(conn, 4), 'big')
-            print("Receiving image of size:", img_len)
-
             data = receive_all(conn, img_len)
 
             with open("received.png", "wb") as f:
                 f.write(data)
 
-    print("Image received")
+            print("Image received")
 
-    payload = extract_payload("received.png")
-    n, t, c = unpack_payload(payload)
+            payload = extract_payload("received.png")
+            n, t, c = unpack_payload(payload)
 
-    recovered = decrypt_data(n, c, t, key)
+            recovered = decrypt_data(n, c, t, key)
+            print("Message from sender:", recovered.decode())
 
-    print("Recovered message:", recovered)
+            reply_key = get_random_bytes(32)
+
+            reply_msg = input("Enter reply message: ").encode()
+
+            nonce, ct, tag = encrypt_data(reply_msg, reply_key)
+            payload = build_payload(nonce, tag, ct)
+
+            embed_payload("input(2048x2048).png", "reply_stego.png", payload)
+
+            with open("reply_stego.png", "rb") as f:
+                img_data = f.read()
+
+            # Send back
+            conn.sendall(len(reply_key).to_bytes(2, 'big') + reply_key)
+            conn.sendall(len(img_data).to_bytes(4, 'big') + img_data)
+
+            print("Reply sent")
 
 
 if __name__ == "__main__":
--- send.py
+++ send.py
@@ -1,26 +1,35 @@
 import socket
 from Crypto.Random import get_random_bytes
 
-from crypto.aes_gcm import encrypt_data
-from utils.helpers import build_payload
+from crypto.aes_gcm import encrypt_data, decrypt_data
+from utils.helpers import build_payload, unpack_payload
 from stego.dct_embed import embed_payload
+from stego.dct_extract import extract_payload
 
 
-HOST = "10.143.57.197" #Laptop IP
+HOST = "10.143.57.197"
 PORT = 5000
+
+
+def receive_all(sock, length):
+    data = b''
+    while len(data) < length:
+        packet = sock.recv(length - len(data))
+        if not packet:
+            raise ConnectionError("Connection lost")
+        data += packet
+    return data
 
 
 def main():
     key = get_random_bytes(32)
 
-    # 🔹 Take message input from user
+    # 🔹 Send message
     user_input = input("Enter message to send: ")
     msg = user_input.encode()
 
     nonce, ct, tag = encrypt_data(msg, key)
     payload = build_payload(nonce, tag, ct)
-
-    print("Payload length:", len(payload))
 
     embed_payload("input(2048x2048).png", "stego.png", payload)
 
@@ -32,13 +41,32 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
-        # Send key
         s.sendall(len(key).to_bytes(2, 'big') + key)
-
-        # Send image
         s.sendall(len(img_data).to_bytes(4, 'big') + img_data)
 
-    print("Image + key sent successfully.")
+        print("Sent image + key")
+
+        print("Waiting for reply...")
+
+        # Receive key
+        key_len = int.from_bytes(receive_all(s, 2), 'big')
+        recv_key = receive_all(s, key_len)
+
+        # Receive image
+        img_len = int.from_bytes(receive_all(s, 4), 'big')
+        img_data = receive_all(s, img_len)
+
+        with open("reply.png", "wb") as f:
+            f.write(img_data)
+
+        print("Reply image received")
+
+    payload = extract_payload("reply.png")
+    n, t, c = unpack_payload(payload)
+
+    recovered = decrypt_data(n, c, t, recv_key)
+
+    print("Reply message:", recovered.decode())
 
 
 if __name__ == "__main__":
